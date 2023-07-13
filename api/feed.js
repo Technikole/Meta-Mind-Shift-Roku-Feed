b0125ac15270a07603c83cc9b82aed89cd967447
NiKole Maxwell <alertnikole@gmail.com>
2023-07-12 21:07:44 -0400

    feed added

---
+++ api/feed.js
@@ -0,0 +1,76 @@
+const axios = require('axios');
+const parseString = require('xml2js').parseString;
+
+axios.get('https://technikole.com/assets/stream/anchor_meta-mind-shift.xml')
+    .then(response => {
+        parseString(response.data, (err, result) => {
+            if (err) {
+                console.error("Failed to parse XML", err);
+                return;
+            }
+
+            const rokuJson = {
+                providerName: "MaxDistro Live",
+                lastUpdated: new Date().toISOString(),
+                language: "en",
+                series: []
+            };
+
+            result.rss.channel[0].item.forEach((item) => {
+                // For each item, convert the XML to the desired JSON format
+
+                var title = item.title[0];
+                var description = item.description[0];
+                var pubDate = item.pubDate[0];
+                var enclosure = item.enclosure[0].$.url;
+                var seasonNumber = parseInt(item["itunes:season"][0]);
+                var episodeNumber = parseInt(item["itunes:episode"][0]);
+
+                // Construct the ID for the episode
+                var guid = "MDL_MetaMindShift_S" + String(seasonNumber).padStart(3, '0') + "E" + String(episodeNumber).padStart(2, '0');
+
+                var episode = {
+                    id: guid,
+                    title: title,
+                    content: {
+                        streamUrl: enclosure,
+                        contentType: "mp4",
+                        language: "en"
+                    },
+                    thumbnail: 'https://d3t3ozftmdmh3i.cloudfront.net/production/podcast_uploaded_nologo/26639417/26639417-1683820734531-2dfdeb65c2daf.jpg',
+                    releaseDate: new Date(pubDate).toISOString(),
+                    shortDescription: description.substring(0, 200),
+                    longDescription: description,
+                };
+
+                var existingSeries = rokuJson.series.find(series => series.title === "Meta Mind Shift Show");
+                if (existingSeries) {
+                    var existingSeason = existingSeries.seasons.find(season => season.seasonNumber === seasonNumber);
+                    if (existingSeason) {
+                        existingSeason.episodes.push(episode);
+                    } else {
+                        existingSeries.seasons.push({
+                            seasonNumber: seasonNumber,
+                            episodes: [episode]
+                        });
+                    }
+                } else {
+                    rokuJson.series.push({
+                        id: "MDL_Meta_Mind_Shift_Bonus",
+                        title: "Meta Mind Shift Show",
+                        seasons: [{
+                            seasonNumber: seasonNumber,
+                            episodes: [episode]
+                        }],
+                        thumbnail: 'https://d3t3ozftmdmh3i.cloudfront.net/production/podcast_uploaded_nologo/26639417/26639417-1683820734531-2dfdeb65c2daf.jpg',
+                        shortDescription: "Join NiKole Maxwell, 'Technikole' the Metaverse Goddess, as she explores the latest trends and insights on how to navigate the Metaverse. We discuss everything from process automation to AI, blockchain, and Web3. It's time for your Meta Mind Shift. ",
+                        genres: ["technology","sci-fi","gaming","science-fiction","educational"]
+                    });
+                }
+            });
+
+            // Log the JSON output
+            console.log(JSON.stringify(rokuJson, null, 2));
+        });
+    })
+    .catch(console.error);
